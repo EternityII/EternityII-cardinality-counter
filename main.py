c7f1089258f6bbf6dbc15185f5ae9b0272cc1936
Chen Fati <fati.chen@etu.umontpellier.fr>
2016-06-30 13:55:16 +0200

    adding export option

--- main.py
+++ main.py
@@ -1,33 +1,34 @@
 # -*- coding: utf-8 -*-
 import argparse
-import os
+# import os # unused
 from sys import stdout
 
 
-def count(file_in_path):
-    dirname = os.path.dirname(file_in_path)
+def count(file_in_path, output):
+    # dirname = os.path.dirname(file_in_path) # unused
 
     file_in = open(file_in_path)
 
     # getting information from first 3 lines
     dataorganisation = file_in.readline()  # ignored
     data = file_in.readline()  # getting information from file
-    datastructure = file_in.readline()
-    datacheck = file_in.readline()
+    datastructure = file_in.readline()  # unused
+    datacheck = file_in.readline()  # unused
 
     data = data.split()
     del data[0]
 
     size = int(data[0])
-    corrotation = int(data[1])
-    pcnumber = int(data[2])
-    pcrotation = int(data[3])
-    posx = int(data[4])
-    posy = int(data[5])
+    # corrotation = int(data[1])  # unused
+    # pcnumber = int(data[2])  # unused
+    # pcrotation = int(data[3])  # unused
+    pos_x = int(data[4])
+    pos_y = int(data[5])
     hamming = int(data[6])
-    nbpieces = int(data[7])
+    nb_pieces = int(data[7])
 
-    ispresent = [[[False for k in xrange(4)] for j in xrange(size * size)] for i in xrange(nbpieces)]
+    is_present = [[[False for k in xrange(4)] for j in xrange(size * size)]
+                  for i in xrange(nb_pieces)]
     stdout.write("processing lines (this may take a while) ")
     stdout.flush()
     for idx, line in enumerate(file_in):
@@ -36,78 +37,91 @@ def count(file_in_path):
             stdout.flush()
 
         line_tab = line.split(";")
-        for depth in xrange(nbpieces):
+        for depth in xrange(nb_pieces):
             pc = line_tab[depth].split(':')
             if pc[0] != '-1':
-                if ispresent[depth][int(pc[0])][int(pc[1])] is False:
-                    ispresent[depth][int(pc[0])][int(pc[1])] = True
+                if is_present[depth][int(pc[0])][int(pc[1])] is False:
+                    is_present[depth][int(pc[0])][int(pc[1])] = True
 
-    counter = [0 for i in xrange(nbpieces)]
-    countpieces(ispresent, counter, nbpieces, size)
-
+    counter = [0 for i in xrange(nb_pieces)]
     plate = [[0 for j in xrange(size)] for i in xrange(size)]
-    putonplate(counter, size, plate, posx, posy, hamming)
+
+    count_pieces(is_present, counter, nb_pieces, size)
+
+    put_on_plate(counter, size, plate, pos_x, pos_y, hamming)
+
+    display_plate(plate, file_in_path, output)
 
 
-def putonplate(counter, size, plate, posx, posy, hamming):
+def put_on_plate(counter, size, plate, pos_x, pos_y, hamming):
     cpt = 0
-    if posx < size:
-        plate[posx][posy] = counter[cpt]
+    if pos_x < size:
+        plate[pos_x][pos_y] = counter[cpt]
     cpt += 1
     for i in xrange(1, hamming + 1):
-        posx += 1
+        pos_x += 1
         for j in xrange(i):
-            if 0 <= posx < size and 0 <= posy < size:
-                plate[posx][posy] = counter[cpt]
+            if 0 <= pos_x < size and 0 <= pos_y < size:
+                plate[pos_x][pos_y] = counter[cpt]
             cpt += 1
-            posx -= 1
-            posy += 1
+            pos_x -= 1
+            pos_y += 1
         for j in xrange(i):
-            if 0 <= posx < size and 0 <= posy < size:
-                plate[posx][posy] = counter[cpt]
+            if 0 <= pos_x < size and 0 <= pos_y < size:
+                plate[pos_x][pos_y] = counter[cpt]
             cpt += 1
-            posx -= 1
-            posy -= 1
+            pos_x -= 1
+            pos_y -= 1
         for j in xrange(i):
-            if 0 <= posx < size and 0 <= posy < size:
-                plate[posx][posy] = counter[cpt]
+            if 0 <= pos_x < size and 0 <= pos_y < size:
+                plate[pos_x][pos_y] = counter[cpt]
             cpt += 1
-            posx += 1
-            posy -= 1
+            pos_x += 1
+            pos_y -= 1
         for j in xrange(i):
-            if 0 <= posx < size and 0 <= posy < size:
-                plate[posx][posy] = counter[cpt]
+            if 0 <= pos_x < size and 0 <= pos_y < size:
+                plate[pos_x][pos_y] = counter[cpt]
             cpt += 1
-            posx += 1
-            posy += 1
+            pos_x += 1
+            pos_y += 1
 
+
+def display_plate(plate, file_in_path, output):
     s = [[str(e) for e in row] for row in plate]
     lens = [max(map(len, col)) for col in zip(*s)]
     fmt = '\t'.join('{{:{}}}'.format(x) for x in lens)
     table = [fmt.format(*row) for row in s]
     stdout.write("\r")
     stdout.flush()
-    print '\n'.join(table)
+    if output:
+        file_out = open("stat_" + file_in_path, "w+")
+        file_out.writelines('\n'.join(table))
+    else:
+        print '\n'.join(table)
 
 
-def countpieces(ispresent, counter, nbpieces, size):
-    for i in xrange(nbpieces):
+def count_pieces(is_present, counter, nb_pieces, size):
+    for i in xrange(nb_pieces):
         for j in xrange(size * size):
             for k in xrange(4):
-                if ispresent[i][j][k]:
+                if is_present[i][j][k]:
                     counter[i] += 1
 
 
 def main():
-    parser = argparse.ArgumentParser(description="The eternity cardinality matrix")
+    parser = argparse.ArgumentParser(
+        description="The eternity cardinality matrix")
 
-    parser.add_argument("files_path", metavar="input-file", type=str, nargs='+', help="The file path")
+    parser.add_argument("files_path", metavar="input-file", type=str,
+                        nargs='+', help="The file path")
+    parser.add_argument("-o", "--output", action="store_true",
+                        help="saves the count in file stat_[input-file]")
 
     args = parser.parse_args()
     # Opening the file
     for file_path in args.files_path:
         print "File : " + file_path
-        count(file_path)
+        count(file_path, args.output)
     return
 
 
